fe190669f59bd638d0e2fc0ae902c5791fca72fb
BagasArdiansaPutra <bagasardiansa06.com>
2026-07-30 21:55:54 +0700

    webGIS update maps

--- app/Filament/Resources/GisFeatures/Schemas/GisFeatureForm.php
+++ app/Filament/Resources/GisFeatures/Schemas/GisFeatureForm.php
@@ -5,8 +5,8 @@ namespace App\Filament\Resources\GisFeatures\Schemas;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\Section;
-use Filament\Forms\Components\Grid;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Schema;
 
 class GisFeatureForm
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'pages.home')->name('home');
 Route::view('/profil', 'pages.profil')->name('profil');
 Route::view('/wisata', 'pages.wisata')->name('wisata');
-Route::view('/peta', 'pages.peta')->name('peta');
+Route::get('/peta', function () {
+    $features = \App\Models\GisFeature::all();
+    return view('pages.peta', compact('features'));
+})->name('peta');
 Route::view('/publikasi', 'pages.publikasi')->name('publikasi');
 Route::view('/umkm', 'pages.umkm')->name('umkm');
