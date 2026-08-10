a70000c6e9379cfb50931ec3374cb01a125491a4
Rakagali Resda Krisandi Putra <faesalgali@gmail.com>
2026-08-09 22:27:06 -0500

    edit card page dropdown

--- app/Filament/Resources/ActivityPhotos/Tables/ActivityPhotosTable.php
+++ app/Filament/Resources/ActivityPhotos/Tables/ActivityPhotosTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\ActivityPhotos\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\DeleteAction;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
@@ -33,6 +34,7 @@ class ActivityPhotosTable
             ])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
