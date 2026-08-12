65801ab18355efdd6936c178f7e614c3bef81bbc
Rakagali Resda Krisandi Putra <faesalgali@gmail.com>
2026-08-12 07:22:36 -0500

    email

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block; font-size: 24px; font-weight: bold; color: #8C5A35; text-decoration: none;">
+{{ config('app.name') }}
+</a>
+</td>
+</tr>
